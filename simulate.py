cf82e06a82ed7c2a6359cb3877a658808695a511
jjbush11 <jjbush@uvm.edu>
2023-01-22 21:24:49 -0500

    Added sleep to slow down

--- simulate.py
+++ simulate.py
@@ -1,5 +1,8 @@
 import pybullet as p
+import time
 physicsClient = p.connect(p.GUI)
-for x in range(1000):
+for x in range(500):
     p.stepSimulation()
+    time.sleep(1/60)
+    print(x)
 p.disconnect
