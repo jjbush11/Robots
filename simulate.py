1bf828dd918369f57bc97ec14bb8e6daad7f673a
jjbush11 <jjbush@uvm.edu>
2023-01-23 14:07:53 -0500

    Added comments and box

--- simulate.py
+++ simulate.py
@@ -1,8 +1,9 @@
 import pybullet as p
 import time
-physicsClient = p.connect(p.GUI)
-for x in range(500):
-    p.stepSimulation()
-    time.sleep(1/60)
+physicsClient = p.connect(p.GUI) #creates object which handles the phyiscs and draws results to GUI (graphical user interface)
+p.loadSDF("box.sdf") #tells pybullet to read in the world described box.sdf
+for x in range(1000):
+    p.stepSimulation() #stes the physics inside the world for a small amount of time
+    time.sleep(1/60) #sleeps the code for 1/60th of a second each loop iteration
     print(x)
 p.disconnect
