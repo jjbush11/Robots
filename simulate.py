0be9ae082c0855a207663045c9c0f6f8dd08117a
jjbush11 <jjbush@uvm.edu>
2023-01-24 22:47:53 -0500

    Added floor

--- simulate.py
+++ simulate.py
@@ -1,7 +1,11 @@
+import pybullet_data #allows for use of plane.urdf
 import pybullet as p
 import time
+#alters how a world is simulated 
 physicsClient = p.connect(p.GUI) #creates object which handles the phyiscs and draws results to GUI (graphical user interface)
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #something to do with floor
 p.setGravity(0,0,-9.8) #sets gravity 
+planeId = p.loadURDF("plane.urdf") #sets floor 
 p.loadSDF("box.sdf") #tells pybullet to read in the world described box.sdf
 for x in range(1000):
     p.stepSimulation() #stes the physics inside the world for a small amount of time
