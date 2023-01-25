73181d1ad5f41d9756f517adbc5f5ed2ea6272e6
jjbush11 <jjbush@uvm.edu>
2023-01-24 22:09:31 -0500

    Added gravity

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import time
 physicsClient = p.connect(p.GUI) #creates object which handles the phyiscs and draws results to GUI (graphical user interface)
+p.setGravity(0,0,-9.8) #sets gravity 
 p.loadSDF("box.sdf") #tells pybullet to read in the world described box.sdf
 for x in range(1000):
     p.stepSimulation() #stes the physics inside the world for a small amount of time
