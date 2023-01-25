ec73fd1e6b3a50cdbf108352c0f42fbeb851a661
jjbush11 <jjbush@uvm.edu>
2023-01-25 17:52:07 -0500

    Added robot body and world

--- generate.py
+++ generate.py
@@ -1,9 +1,20 @@
 import pyrosim.pyrosim as pyrosim #makes it so we dont have to say pyrosim.pyrosim.Start... every time
 #alters what is in the world
-pyrosim.Start_SDF("world.sdf") #tells pyrosim name of file where info about the world should be stored
+
 length =1
 width=1
 height=1
-pyrosim.Send_Cube(name="Box", pos=[0,0,.5] , size=[length,width,height]) #creates box with initial size and positons 
 
-pyrosim.End() #closes sdf file
+def Create_World():
+    pyrosim.Start_SDF("world.sdf") #tells pyrosim name of file where info about the world should be stored
+    pyrosim.Send_Cube(name="Box", pos=[-2,2,.5] , size=[length,width,height]) #creates box with initial size and positons 
+    pyrosim.End() #closes sdf file
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,.5] , size=[length,width,height]) #creates box with initial size and positons 
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
+
--- simulate.py
+++ simulate.py
@@ -5,8 +5,9 @@ import time
 physicsClient = p.connect(p.GUI) #creates object which handles the phyiscs and draws results to GUI (graphical user interface)
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #something to do with floor
 p.setGravity(0,0,-9.8) #sets gravity 
-planeId = p.loadURDF("plane.urdf") #sets floor 
-p.loadSDF("box.sdf") #tells pybullet to read in the world described box.sdf
+planeId = p.loadURDF("plane.urdf") #sets floor
+robotID = p.loadURDF("body.urdf") #sets floor 
+p.loadSDF("world.sdf") #tells pybullet to read in the world described box.sdf
 for x in range(1000):
     p.stepSimulation() #stes the physics inside the world for a small amount of time
     time.sleep(1/60) #sleeps the code for 1/60th of a second each loop iteration
