b56b2409f90e5def5226a56493b42a6133239ec2
jjbush11 <jjbush@uvm.edu>
2023-01-24 23:12:42 -0500

    Made cube start even with ground and make cube different size

--- generate.py
+++ generate.py
@@ -1,4 +1,8 @@
 import pyrosim.pyrosim as pyrosim #makes it so we dont have to say pyrosim.pyrosim.Start... every time
+#alters what is in the world
 pyrosim.Start_SDF("box.sdf") #tells pyrosim name of file where info about the world should be stored
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1]) #creates box with initial size and positons 
+length =1
+width=2
+height=3
+pyrosim.Send_Cube(name="Box", pos=[0,0,1.5] , size=[length,width,height]) #creates box with initial size and positons 
 pyrosim.End() #closes sdf file
