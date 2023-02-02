ba65fb870104f73ce0bd5168167d8622a1754834
jjbush11 <jjbush@uvm.edu>
2023-02-02 14:02:00 -0500

    sensors values printing

--- simulate.py
+++ simulate.py
@@ -1,6 +1,9 @@
 import pybullet_data #allows for use of plane.urdf
 import pybullet as p
+import pyrosim.pyrosim as pyrosim 
 import time
+import numpy
+
 #alters how a world is simulated 
 physicsClient = p.connect(p.GUI) #creates object which handles the phyiscs and draws results to GUI (graphical user interface)
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #something to do with floor
@@ -8,8 +11,13 @@ p.setGravity(0,0,-9.8) #sets gravity
 planeId = p.loadURDF("plane.urdf") #sets floor
 robotID = p.loadURDF("body.urdf") #sets floor 
 p.loadSDF("world.sdf") #tells pybullet to read in the world described box.sdf
+pyrosim.Prepare_To_Simulate(robotID) #does more setting up
+backLegSensorValues = numpy.zeros(10000)
+print(backLegSensorValues)
 for x in range(1000):
     p.stepSimulation() #stes the physics inside the world for a small amount of time
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg") #adds sensor 
+    print(backLegTouch)
     time.sleep(1/60) #sleeps the code for 1/60th of a second each loop iteration
-    print(x)
+    #print(x) prints the time steps
 p.disconnect
