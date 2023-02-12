21605886b0472ee15bfa0effa0af21a5a3dead6e
jjbush11 <jjbush@uvm.edu>
2023-02-12 12:30:37 -0500

    Fixed keyerror by adding b'name'

--- simulate.py
+++ simulate.py
@@ -16,9 +16,17 @@ pyrosim.Prepare_To_Simulate(robotID) #does more setting up
 backLegSensorValues = numpy.zeros(1000)
 frontLegSensorValues = numpy.zeros(1000)
 for x in range(1000):
-    p.stepSimulation() #stes the physics inside the world for a small amount of time
+    p.stepSimulation() #sets the physics inside the world for a small amount of time
     backLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg") #adds sensor 
-    frontLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg") #adds sensor 
+    frontLegSensorValues[x] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg") #adds sensor
+
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robotID, #simlaute a motor in robot named robot
+    jointName = b'Torso_BackLeg', #motors are always attached to joints
+    controlMode = p.POSITION_CONTROL, #mains ones are position or velocity
+    targetPosition = 0.0,
+    maxForce = 500)
+
     time.sleep(1/60) #sleeps the code for 1/60th of a second each loop iteration
 
 print(frontLegSensorValues)
