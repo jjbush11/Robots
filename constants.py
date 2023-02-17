c9e921ab37e5a6e891285b10148fa6622cacf577
jjbush11 <jjbush@uvm.edu>
2023-02-16 20:29:02 -0500

    Created constant.py and replaced values in simualte

---
+++ constants.py
@@ -0,0 +1,13 @@
+import numpy
+
+frontLegAmplitude = numpy.pi/4.
+frontLegFrequency = 10
+frontLegPhaseOffset = 0
+
+backLegAmplitude = numpy.pi/4.
+backLegFrequency = 10
+backLegPhaseOffset = numpy.pi/4
+
+loop=1000
+gravity=-9.8
+motorForce=40
