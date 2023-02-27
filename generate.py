babf2e2f622b14b97da8a7763d852d8ef0ccfa8d
jjbush11 <jjbush@uvm.edu>
2023-02-27 13:49:07 -0500

    First commit, created generate body func

--- generate.py
+++ generate.py
@@ -10,7 +10,10 @@ def Create_World():
     pyrosim.Send_Cube(name="Box", pos=[-2,2,.5] , size=[length,width,height]) #creates box with initial size and positons 
     pyrosim.End() #closes sdf file
 
-def Create_Robot():
+# def Create_Robot():
+#     pass
+
+def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
 
     pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[length,width,height]) #creates box with initial size and positons 
@@ -19,12 +22,24 @@ def Create_Robot():
     pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
     pyrosim.Send_Cube(name="FrontLeg", pos=[.5,0,-.5] , size=[length,width,height]) #creates box with initial size and positons
 
+    pyrosim.End()
 
+def Generate_Brain():
+    pyrosim.Start_URDF("body.urdf")
+
+    pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[length,width,height]) #creates box with initial size and positons 
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-.5,0,-.5] , size=[length,width,height]) #creates box with initial size and positons 
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[.5,0,-.5] , size=[length,width,height]) #creates box with initial size and positons
 
     pyrosim.End()
 
+
+
 Create_World()
-Create_Robot()
+Generate_Body()
+# Create_Robot()
 
     # pyrosim.Send_Cube(name="Link0", pos=[0,0,.5] , size=[length,width,height]) #creates box with initial size and positons 
     # pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
